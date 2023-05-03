f7b424baa949a94305df69f57dd7aac0577fb806
Anastasia Smirnova <smirnova.anastasia.k@gmail.com>
2023-05-03 16:10:20 +0300

    Delete image added

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,18 +1,22 @@
-import React, { useState, Component } from "react";
+import React, { useState } from "react";
 import GalleryItem from "./GalleryItem";
 
 export const Gallery = ({}) => {
   const [items, setItems] = useState([
-    { src: "./images/one.jpg", title: "one" },
-    { src: "./images/two.jpg", title: "two" },
-    { src: "./images/three.jpg", title: "three" },
-    { src: "./images/four.jpg", title: "four" },
+    { src: "./images/one.jpg", title: "one", id: 1 },
+    { src: "./images/two.jpg", title: "two", id: 2 },
+    { src: "./images/three.jpg", title: "three", id: 3 },
+    { src: "./images/four.jpg", title: "four", id: 4 },
   ]);
+
+  const removeItem = (id) => {
+    setItems(items.filter((item) => item.id !== id));
+  };
 
   return (
     <div className="images">
       {items.map((item, index) => (
-        <GalleryItem item={item} key={index} />
+        <GalleryItem item={item} key={index} onDelete={removeItem} />
       ))}
     </div>
   );
